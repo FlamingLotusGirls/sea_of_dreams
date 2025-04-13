ba36f45a02673c9cefa9fd36d525417b3359baeb
Pixelprizm <pixelprizm@gmail.com>
2025-04-12 17:30:37 -0700

    Send bang protocol command, it works!

--- new/server/src/main.rs
+++ new/server/src/main.rs
@@ -1,4 +1,5 @@
-use serialport::{self, SerialPortType};
+use serialport::{self, DataBits, SerialPortType, StopBits};
+use std::io::Write as _;
 
 fn main() {
     match serialport::available_ports() {
@@ -9,10 +10,10 @@ fn main() {
         Ok(mut available_ports) => {
             available_ports.sort_by_key(|key| key.port_name.clone());
 
-            for port in available_ports {
+            for port in &available_ports {
                 println!("{}", port.port_name);
 
-                match port.port_type {
+                match &port.port_type {
                     SerialPortType::UsbPort(info) => {
                         println!("        Type: USB");
                         println!("        VID: {:04x}", info.vid);
@@ -40,6 +41,27 @@ fn main() {
                         println!("        Type: Unknown");
                     }
                 }
+            }
+
+            if let Some(port) = available_ports.get(0) {
+                let mut port = serialport::new(&port.port_name, 19200)
+                    .stop_bits(StopBits::One)
+                    .data_bits(DataBits::Eight)
+                    .open()
+                    .unwrap_or_else(|e| {
+                        eprintln!("Failed to open port {}. Error: {}", port.port_name, e);
+                        ::std::process::exit(1);
+                    });
+
+                let command = "!0121.";
+                match port.write(command.as_bytes()) {
+                    Ok(_) => {
+                        print!("{}", command);
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Err(e) => eprintln!("{e:?}"),
+                }
+                println!();
             }
         }
     }
