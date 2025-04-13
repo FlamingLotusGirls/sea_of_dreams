0f368bbc12a98867aa6bfd1e2d7561e3723ef1e0
Pixelprizm <pixelprizm@gmail.com>
2025-04-12 17:15:26 -0700

    Print available serial ports and info (from serialport crate example)

--- new/server/src/main.rs
+++ new/server/src/main.rs
@@ -1,3 +1,46 @@
+use serialport::{self, SerialPortType};
+
 fn main() {
-    println!("Hello, world!");
+    match serialport::available_ports() {
+        Err(e) => {
+            eprintln!("Error listing serial ports:");
+            eprintln!("{e:?}");
+        }
+        Ok(mut available_ports) => {
+            available_ports.sort_by_key(|key| key.port_name.clone());
+
+            for port in available_ports {
+                println!("{}", port.port_name);
+
+                match port.port_type {
+                    SerialPortType::UsbPort(info) => {
+                        println!("        Type: USB");
+                        println!("        VID: {:04x}", info.vid);
+                        println!("        PID: {:04x}", info.pid);
+                        println!(
+                            "        Serial Number: {}",
+                            info.serial_number.as_ref().map_or("", String::as_str)
+                        );
+                        println!(
+                            "        Manufacturer: {}",
+                            info.manufacturer.as_ref().map_or("", String::as_str)
+                        );
+                        println!(
+                            "        Product: {}",
+                            info.product.as_ref().map_or("", String::as_str)
+                        );
+                    }
+                    SerialPortType::BluetoothPort => {
+                        println!("        Type: Bluetooth");
+                    }
+                    SerialPortType::PciPort => {
+                        println!("        Type: PCI");
+                    }
+                    SerialPortType::Unknown => {
+                        println!("        Type: Unknown");
+                    }
+                }
+            }
+        }
+    }
 }
