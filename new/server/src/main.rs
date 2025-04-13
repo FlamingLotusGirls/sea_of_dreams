186d0fb73eea7b8976572bbea45134b7c40e57bf
Pixelprizm <pixelprizm@gmail.com>
2025-04-13 01:19:49 -0700

    Move serialport stuff to a new output_poofer_bus file

--- new/server/src/main.rs
+++ new/server/src/main.rs
@@ -1,97 +1,22 @@
 mod effects;
 mod output_artnet;
+mod output_poofer_bus;
 mod preview;
 
 use iced::{
     ContentFit, Element, Length, Settings, Size, Subscription, Task, Theme, application, time,
     window,
 };
-use serialport::{self, DataBits, SerialPort, SerialPortType, StopBits};
-use std::{
-    io::Write as _,
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
 use effects::{Effect, get_effect};
 use output_artnet::OutputArtnetSocket;
-
-fn main() -> iced::Result {
-    match serialport::available_ports() {
-        Err(e) => {
-            eprintln!("Error listing serial ports:");
-            eprintln!("{e:?}");
-            ::std::process::exit(1);
-        }
-        Ok(mut available_ports) => {
-            available_ports.sort_by_key(|key| key.port_name.clone());
-
-            for port in &available_ports {
-                println!("{}", port.port_name);
-
-                match &port.port_type {
-                    SerialPortType::UsbPort(info) => {
-                        println!("        Type: USB");
-                        println!("        VID: {:04x}", info.vid);
-                        println!("        PID: {:04x}", info.pid);
-                        println!(
-                            "        Serial Number: {}",
-                            info.serial_number.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "        Manufacturer: {}",
-                            info.manufacturer.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "        Product: {}",
-                            info.product.as_ref().map_or("", String::as_str)
-                        );
-                    }
-                    SerialPortType::BluetoothPort => {
-                        println!("        Type: Bluetooth");
-                    }
-                    SerialPortType::PciPort => {
-                        println!("        Type: PCI");
-                    }
-                    SerialPortType::Unknown => {
-                        println!("        Type: Unknown");
-                    }
-                }
-            }
-
-            if let Some(port) = available_ports.get(0) {
-                let port = serialport::new(&port.port_name, 19200)
-                    .stop_bits(StopBits::One)
-                    .data_bits(DataBits::Eight)
-                    .open()
-                    .unwrap_or_else(|e| {
-                        eprintln!("Failed to open port {}. Error: {}", port.port_name, e);
-                        ::std::process::exit(1);
-                    });
-
-                return run(port);
-            } else {
-                eprintln!("Error getting first serial port.");
-                ::std::process::exit(1);
-            }
-        }
-    }
-}
 
 // Since we only support one art-net universe (512B), 170 is the maximum number of total pixels for now
 const ELDER_COUNT: usize = 9;
 const FRAME_OUTPUT_PERIOD: usize = 2;
 
-fn run(mut port: Box<dyn SerialPort>) -> iced::Result {
-    let command = "!0121.";
-    match port.write(command.as_bytes()) {
-        Ok(_) => {
-            print!("{}", command);
-            std::io::stdout().flush().unwrap();
-        }
-        Err(e) => eprintln!("{e:?}"),
-    }
-    println!();
-
+fn main() -> iced::Result {
     application("Haven Server", App::update, App::view)
         .theme(|_| Theme::Dark)
         .settings(Settings {
@@ -251,6 +176,11 @@ pub struct Poofer {
     pub relays: Vec<RelayAddress>,
 }
 
+/**
+ * Board address is based on dip switches. LSB is dip switch 1, MSB is dip switch 5. Putting the
+ * switch in the direction of the arrow means 0; against the opposite direction of the arrow means
+ * 1.
+ */
 #[derive(Clone, Debug)]
 pub struct RelayAddress {
     pub board_address: u8,
---
+++ new/server/src/output_poofer_bus.rs
@@ -0,0 +1,95 @@
+use crate::{Elder, RelayAddress};
+use serialport::{DataBits, SerialPort, SerialPortType, StopBits};
+use std::io::Write as _;
+
+pub struct PooferBusPort {
+    port: Box<dyn SerialPort>,
+}
+impl PooferBusPort {
+    pub fn available_ports() -> Vec<String> {
+        match serialport::available_ports() {
+            Err(e) => {
+                eprintln!("Error listing serial ports:");
+                eprintln!("{e:?}");
+                ::std::process::exit(1);
+            }
+            Ok(mut available_ports) => {
+                available_ports.sort_by_key(|key| key.port_name.clone());
+
+                available_ports
+                    .iter()
+                    .filter_map(|port| {
+                        println!("{}", port.port_name);
+
+                        match &port.port_type {
+                            SerialPortType::UsbPort(info) => {
+                                println!("  Type: USB");
+                                println!("  VID: {:04x}", info.vid);
+                                println!("  PID: {:04x}", info.pid);
+                                println!(
+                                    "  Serial Number: {}",
+                                    info.serial_number.as_ref().map_or("", String::as_str)
+                                );
+                                println!(
+                                    "  Manufacturer: {}",
+                                    info.manufacturer.as_ref().map_or("", String::as_str)
+                                );
+                                println!(
+                                    "  Product: {}",
+                                    info.product.as_ref().map_or("", String::as_str)
+                                );
+                            }
+                            SerialPortType::BluetoothPort => {
+                                println!("  Type: Bluetooth");
+                            }
+                            SerialPortType::PciPort => {
+                                println!("  Type: PCI");
+                            }
+                            SerialPortType::Unknown => {
+                                println!("  Type: Unknown");
+                            }
+                        }
+                        if matches!(port.port_type, SerialPortType::UsbPort(_)) {
+                            Some("a".to_string())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<String>>()
+            }
+        }
+    }
+    pub fn new(serial_port_name: &str) -> Self {
+        let port = serialport::new(serial_port_name, 19200)
+            .stop_bits(StopBits::One)
+            .data_bits(DataBits::Eight)
+            .open()
+            .unwrap_or_else(|e| {
+                eprintln!("Failed to open port {}. Error: {}", serial_port_name, e);
+                ::std::process::exit(1);
+            });
+
+        Self { port }
+    }
+
+    pub fn output(&mut self, elders: &Vec<Elder>) {
+        for elder in elders {
+            let on_digit = elder.poofer.on as u8;
+            for RelayAddress {
+                board_address,
+                poofer_address,
+            } in &elder.poofer.relays
+            {
+                let command = format!("!{board_address:02}{poofer_address}{on_digit}.");
+                match self.port.write(command.as_bytes()) {
+                    Ok(_) => {
+                        print!("{}", command);
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Err(e) => eprintln!("{e:?}"),
+                }
+            }
+        }
+        println!();
+    }
+}
